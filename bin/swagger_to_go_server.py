a73995627e783c63d2a72f7b327ef95a661e95fe
Marko Ristin <marko.ristin@gmail.com>
2018-12-06 08:18:51 +0100

    swagger_to_go_server casts args and uses pathlib (#31)

--- bin/swagger_to_go_server.py
+++ bin/swagger_to_go_server.py
@@ -3,8 +3,9 @@
 Reads a correct swagger file and produces Go code
 """
 import argparse
-import os
+import pathlib
 import sys
+from typing import cast, TextIO
 
 import swagger_to.go_server
 import swagger_to.intermediate
@@ -12,9 +13,8 @@ import swagger_to.swagger
 
 
 def main() -> None:
-    """"
-    Main routine
-    """
+    """Executes the main routine."""
+    # pylint: disable=too-many-locals
     parser = argparse.ArgumentParser("Reads a correct swagger file and produces Go code")
     parser.add_argument("--swagger_path", help="path to the swagger file", required=True)
     parser.add_argument("--outdir", help="path to the output directory", required=True)
@@ -27,23 +27,27 @@ def main() -> None:
     assert isinstance(args.outdir, str)
     assert isinstance(args.swagger_path, str)
 
-    if not os.path.exists(args.swagger_path):
-        print("Swagger file does not exist: {}".format(args.swagger_path), file=sys.stderr)
+    force = bool(args.force)
+    no_samples = bool(args.no_samples)
+    swagger_path = pathlib.Path(args.swagger_path)
+    outdir = pathlib.Path(args.outdir)
+
+    if not swagger_path.exists():
+        print("Swagger file does not exist: {}".format(swagger_path), file=sys.stderr)
         sys.exit(1)
 
-    if not os.path.exists(args.outdir):
-        os.makedirs(args.outdir)
+    outdir.mkdir(parents=True, exist_ok=True)
 
-    if not args.force:
+    if not force:
         for fname in ['main.go', 'handlers.go', 'types.go', 'routes.go', 'jsonschemas.go']:
-            pth = os.path.join(args.outdir, fname)
-            if os.path.exists(pth):
-                print("File exists, but --force was not specified: {!r}".format(pth))
+            pth = outdir / fname
+            if pth.exists():
+                print("File exists, but --force was not specified: {!r}".format(pth), file=sys.stderr)
                 sys.exit(1)
 
-    swagger, errs = swagger_to.swagger.parse_yaml_file(path=args.swagger_path)
+    swagger, errs = swagger_to.swagger.parse_yaml_file(path=swagger_path.as_posix())
     if errs:
-        raise AssertionError("Errors in {}:\n{}".format(args.swagger_path, "\n".join(errs)))
+        raise AssertionError("Errors in {}:\n{}".format(swagger_path, "\n".join(errs)))
 
     intermediate_typedefs = swagger_to.intermediate.to_typedefs(swagger=swagger)
     intermediate_params = swagger_to.intermediate.to_parameters(swagger=swagger, typedefs=intermediate_typedefs)
@@ -56,28 +60,24 @@ def main() -> None:
 
     package = swagger.name
 
-    pth = os.path.join(args.outdir, 'types.go')
-    with open(pth, 'wt') as fid:
-        swagger_to.go_server.write_types_go(package=package, typedefs=go_typedefs, fid=fid)
+    with (outdir / 'types.go').open('wt') as fid:
+        swagger_to.go_server.write_types_go(package=package, typedefs=go_typedefs, fid=cast(TextIO, fid))
 
-    pth = os.path.join(args.outdir, 'routes.go')
-    with open(pth, 'wt') as fid:
-        swagger_to.go_server.write_routes_go(package=package, routes=go_routes, fid=fid)
+    with (outdir / 'routes.go').open('wt') as fid:
+        swagger_to.go_server.write_routes_go(package=package, routes=go_routes, fid=cast(TextIO, fid))
 
-    pth = os.path.join(args.outdir, 'handler.go')
-    with open(pth, 'wt') as fid:
-        swagger_to.go_server.write_handler_go(package=package, routes=go_routes, fid=fid)
+    with (outdir / 'handler.go').open('wt') as fid:
+        swagger_to.go_server.write_handler_go(package=package, routes=go_routes, fid=cast(TextIO, fid))
 
-    if not args.no_samples:
-        pth = os.path.join(args.outdir, 'handler_impl.go.sample')
-        with open(pth, 'wt') as fid:
-            swagger_to.go_server.write_handler_impl_go(package=package, routes=go_routes, fid=fid)
+    if not no_samples:
+        with (outdir / 'handler_impl.go.sample').open('wt') as fid:
+            swagger_to.go_server.write_handler_impl_go(package=package, routes=go_routes, fid=cast(TextIO, fid))
 
-    pth = os.path.join(args.outdir, 'jsonschemas.go')
-    with open(pth, 'wt') as fid:
-        swagger_to.go_server.write_json_schemas_go(package=package, routes=go_routes, typedefs=go_typedefs, fid=fid)
+    with (outdir / 'jsonschemas.go').open('wt') as fid:
+        swagger_to.go_server.write_json_schemas_go(
+            package=package, routes=go_routes, typedefs=go_typedefs, fid=cast(TextIO, fid))
 
-    print("Generated go server code in: {}".format(args.outdir))
+    print("Generated go server code in: {}".format(outdir))
 
 
 if __name__ == "__main__":
