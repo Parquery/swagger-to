8508f1f289938d7cf63069eecb501a543e58ddc9
Marko Ristin <marko.ristin@gmail.com>
2018-12-23 09:09:34 +0100

    added test case with files to go server and py client (#46)

---
+++ tests/cases/go_server/files/routes.go
@@ -0,0 +1,53 @@
+package test_server
+
+// Automatically generated file by swagger_to. DO NOT EDIT OR APPEND ANYTHING!
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+// SetupRouter sets up a router. If you don't use any middleware, you are good to go.
+// Otherwise, you need to maually re-implement this function with your middlewares.
+func SetupRouter(h Handler) *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc(`/upload`,
+		func(w http.ResponseWriter, r *http.Request) {
+			WrapUpload(h, w, r)
+		}).Methods("put")
+
+	r.HandleFunc(`/{path:.+}`,
+		func(w http.ResponseWriter, r *http.Request) {
+			WrapStatic(h, w, r)
+		}).Methods("get")
+
+	return r
+}
+
+// WrapUpload wraps the path `/upload` with the method "put"
+func WrapUpload(h Handler, w http.ResponseWriter, r *http.Request) {
+	h.Upload(w, r)
+}
+
+// WrapStatic wraps the path `/{path:.+}` with the method "get"
+//
+// Path description:
+// serves a static file that matches the path.
+func WrapStatic(h Handler, w http.ResponseWriter, r *http.Request) {
+	var aPath string
+
+	vars := mux.Vars(r)
+
+	if _, ok := vars["path"]; !ok {
+		http.Error(w, "Parameter 'path' expected in path", http.StatusBadRequest)
+		return
+	}
+	aPath = vars["path"]
+
+	h.Static(w,
+		r,
+		aPath)
+}
+
+// Automatically generated file by swagger_to. DO NOT EDIT OR APPEND ANYTHING!
---
+++ tests/cases/py_client/files/client.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# Automatically generated file by swagger_to. DO NOT EDIT OR APPEND ANYTHING!
+"""Implements the client for test_server."""
+
+# pylint: skip-file
+# pydocstyle: add-ignore=D105,D107,D401
+
+import contextlib
+from typing import Any, BinaryIO, Dict, List, MutableMapping, Optional
+
+import requests
+import requests.auth
+
+
+class RemoteCaller:
+    """Executes the remote calls to the server."""
+
+    def __init__(self, url_prefix: str, auth: Optional[requests.auth.AuthBase] = None) -> None:
+        self.url_prefix = url_prefix
+        self.auth = auth
+
+    def upload(
+            self,
+            file_nme: str,
+            reference_image: BinaryIO) -> bytes:
+        """
+        Send a put request to /upload.
+
+        :param file_nme: identifies the uploaded file.
+        :param reference_image: contains a .tar archive containing the reference image(s) encoded in JPEG.
+        :return: states that the session was correctly updated.
+        """
+        url = self.url_prefix + '/upload'
+
+        data = {
+            'file_nme': file_nme}
+
+        files = {
+            'reference_image': reference_image}
+
+        resp = requests.request(
+            method='put',
+            url=url,
+            data=data,
+            files=files,
+            auth=self.auth)
+
+        with contextlib.closing(resp):
+            resp.raise_for_status()
+            return resp.content
+
+    def static(
+            self,
+            path: str) -> bytes:
+        """
+        serves a static file that matches the path.
+
+        :param path: is the path to the file relative to the root.
+        :return: serves the file content.
+        """
+        url = "".join([
+            self.url_prefix,
+            '/',
+            str(path)])
+
+        resp = requests.request(
+            method='get',
+            url=url,
+            auth=self.auth)
+
+        with contextlib.closing(resp):
+            resp.raise_for_status()
+            return resp.content
+
+
+# Automatically generated file by swagger_to. DO NOT EDIT OR APPEND ANYTHING!
