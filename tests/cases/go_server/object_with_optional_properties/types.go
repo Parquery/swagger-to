56c39e7dd63e515bf2cdff0c14f19d23252de1fb
Marko Ristin <marko.ristin@gmail.com>
2018-12-23 08:37:04 +0100

    added test case with optional properties to go server and py client (#44)

---
+++ tests/cases/go_server/object_with_optional_properties/types.go
@@ -0,0 +1,13 @@
+package test
+
+// Automatically generated file by swagger_to. DO NOT EDIT OR APPEND ANYTHING!
+
+// Capacity is an identifiable primitive definition.
+type Capacity int32
+
+// TestObject is a test object.
+type TestObject struct {
+	// is a test string property.
+	ProductID *string `json:"product_id,omitempty"`
+
+	Capacity *Capacity `json:"capacity,omitempty"`}
